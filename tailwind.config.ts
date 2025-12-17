8717c991b400cb188ba7642874e95395572f768b
roshan mef <roshan.mef@gmail.com>
2025-12-17 09:19:37 +0000

    use that font everywhere

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
+        body: ['"Bebas Neue"', 'sans-serif'],
         headline: ['"Bebas Neue"', 'sans-serif'],
         code: ['monospace'],
       },
