a88bb10d574702f1af36ebf2a5e188ffaa3a091f
roshan mef <roshan.mef@gmail.com>
2025-12-17 08:37:31 +0000

    Nav looks doubled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Header from '@/components/layout/header';
-import Footer from '@/components/layout/footer';
 import HeroSection from '@/components/sections/hero';
 import AiSuggesterSection from '@/components/sections/ai-suggester';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-svh">
-      <Header />
-      <main className="flex-1">
-        <HeroSection />
-        <AiSuggesterSection />
-      </main>
-      <Footer />
-    </div>
+    <main className="flex-1">
+      <HeroSection />
+      <AiSuggesterSection />
+    </main>
   );
 }
